3a6b5f66151bf17c50289f73965e25badf013709
Paul Hostert <paul.hostert@googlemail.com>
2025-06-10 17:00:12 +0200

    Planning Poker zu Sidebar hinzufügen

--- init.rb
+++ init.rb
@@ -12,13 +12,4 @@ Redmine::Plugin.register :openproject_planning_poker do
   description 'Erweitert OpenProject um Planning Poker'
   url 'https://github.com/Paul-Hostert/openproject-planning_poker'
   requires_openproject '>= 13.0.0'
-  
-  menu :project_menu,
-       :planning_poker,
-       { controller: '/planning_poker', action: 'index' },
-       caption: 'Planning Poker',
-       after: :work_packages,
-       icon: 'icon-star',
-       param: :project_id,
-       if: Proc.new { |p| p.module_enabled?('planning_poker') }
 end
--- lib/openproject/planning_poker/engine.rb
+++ lib/openproject/planning_poker/engine.rb
@@ -2,26 +2,30 @@ module OpenProject
   module PlanningPoker
     class Engine < ::Rails::Engine
       engine_name :openproject_planning_poker
-
       include OpenProject::Plugins::ActsAsOpEngine
-
+      
       register 'openproject-planning_poker',
                author_url: 'https://github.com/Paul-Hostert',
                requires_openproject: '>= 13.0.0' do
         
-
         project_module :planning_poker, name: 'Planning Poker' do
           permission :view_planning_poker,
                      { planning_poker: [:index, :vote, :show_results, :show_stories] },
                      permissible_on: :project,
                      public: true,
                      require: :loggedin
-                     
           permission :manage_planning_poker,
                      { planning_poker: [:start_session, :next_story, :join_session, :save_story_points, :restart_session] },
                      permissible_on: :project,
                      require: :member
         end
+        
+        menu :project_menu,
+             :planning_poker,
+             { controller: '/planning_poker', action: 'index' },
+             caption: 'Planning Poker',
+             after: :work_packages,
+             icon: 'star'
       end
     end
   end
